698a749db871be6448cee122286294302349fb94
MasterOz786 <hassanejaz400@gmail.com>
2025-06-24 07:00:15 +0500

    test2

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+console.log(rootElement);
 if (!rootElement) {
   console.error('Root element with id="root" not found!');
   document.body.innerHTML = '<div style="color:red;font-size:2rem;text-align:center;margin-top:2rem;">Root element with id="root" not found!</div>';
